2da2f4ef8a645f9d2041ab3f9237525bc9f9a82c
jkmcpherson <jkmcpherson@uh.edu>
2022-11-25 19:26:35 -0600

    updates

--- app.r
+++ app.r
@@ -1,13 +1,14 @@
 # sinlge-file app.r from https://mastering-shiny.org/basic-app.html
 library(shiny)
+
+
 ui <- fluidPage(
   "Hello, world!"
 )
+
+
 server <- function(input, output, session) {
 }
-shinyApp(ui, server)
 
-#publish to shinyapps.io using https://shiny.rstudio.com/articles/shinyapps.html section: "Deploying a Shiny app"
-library(rsconnect)
-deployApp()
-#publishing to shinyapps.io in RStudio easier, thanks to https://statsandr.com/blog/how-to-publish-shiny-app-example-with-shinyapps-io/
+# Return a Shiny app object 
+shinyApp(ui = ui, server = server)
