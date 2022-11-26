d838bfc6f24aae86f7bccbe775e7e900d8aabd27
jkmcpherson <jkmcpherson@uh.edu>
2022-11-25 19:26:45 -0600

    updates

--- app.r
+++ app.r
@@ -1,11 +1,10 @@
 # sinlge-file app.r from https://mastering-shiny.org/basic-app.html
-library(shiny)
-
+#shiny::runApp("app.r") to run the app
+#rsconnect::deployApp() to deploy app in shinyapps.io
 
 ui <- fluidPage(
   "Hello, world!"
 )
-
 
 server <- function(input, output, session) {
 }
